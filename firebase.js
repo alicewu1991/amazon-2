b6a4efe15382dc812f56206ea746d0476c838b5b
alicewu1991 <alicewucs@gmail.com>
2023-01-05 21:18:35 +0800

    nextauth

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyCMfxX4Uekx_oNUt8wAG4hSx88gN4idNWg",
+	authDomain: "amaz-2-81fb1.firebaseapp.com",
+	projectId: "amaz-2-81fb1",
+	storageBucket: "amaz-2-81fb1.appspot.com",
+	messagingSenderId: "778273243977",
+	appId: "1:778273243977:web:f59515b9a3fd00b12c1098",
+};
