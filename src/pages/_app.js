858509d48b9dfca4630494ccba4b884b6fa6e4e8
alicewu1991 <alicewucs@gmail.com>
2023-04-18 13:18:07 +0800

    add Google Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,10 +2,12 @@ import { Provider } from "react-redux";
 import { store } from "../app/store";
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react"; //身份授權 給整個應用程序訪問下一個身份驗證狀態
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 const MyApp = ({ Component, pageProps, session }) => {
 	return (
 		<SessionProvider session={session}>
+			<GoogleAnalytics />
 			<Provider store={store}>
 				<Component {...pageProps} />
 			</Provider>
